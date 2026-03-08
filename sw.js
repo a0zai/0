ae20b8f38f14901c5f98666b9f9fbfd1ec040ccc
0 <201962374+a0zai@users.noreply.github.com>
2026-03-08 05:25:11 +0100

    W

--- sw.js
+++ sw.js
@@ -2,16 +2,33 @@ const cacheName = 'v1-0-origin';
 const assets = [
   './',
   './index.html',
+  './manifest.json',
   './0.PNG',
   'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js',
   'https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.fog.min.js',
   'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
